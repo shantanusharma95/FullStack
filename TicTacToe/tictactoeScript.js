4966adbfe19fbe47af8373a807dc023b94e8874e
Shantanu Sharma <shantanu.feb17@gmail.com>
2020-06-16 11:55:28 +0530

    Create tictactoeScript.js

---
+++ TicTacToe/tictactoeScript.js
@@ -0,0 +1,30 @@
+//Restart game button
+var restart = document.querySelector("#b")
+
+//Grab all the squares
+var squares = document.querySelectorAll('td')
+
+//Clear all the squares
+function clearBoard(){
+    for (var i = 0; i < squares.length; i++){
+        squares[i].textContent = '';
+    }
+}
+
+restart.addEventListener('click', clearBoard);
+
+//Check the square marker
+function changeMarker(){
+    if(this.textContent === ''){
+        this.textContent = 'X'
+    }else if (this.textContent === 'X'){
+        this.textContent = 'O'
+    }else [
+        this.textContent = ''
+    ]
+}
+
+//For loop to add event listeners to all the squares
+for (var i = 0; i < squares.length; i++){
+    squares[i].addEventListener('click',changeMarker)
+}
