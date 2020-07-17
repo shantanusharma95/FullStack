4d08931b966f8a822172e16aa039339324b2c6b5
Shantanu Sharma <shantanu.feb17@gmail.com>
2020-07-17 17:36:01 +0530

    Create toDoApp.js

---
+++ ToDo App/toDoApp.js
@@ -0,0 +1,61 @@
+// creating a "close" button and adding to each list item
+var itemList = document.getElementsByTagName("LI");
+for( var i = 0; i < itemList.length; i++){
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    itemList[i].appendChild(span);
+    console.log("hello");
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+    }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var chkList = document.querySelector('ul');
+chkList.addEventListener('click',function(ev){
+    if (ev.target.tagName === 'LI'){
+        ev.target.classList.toggle('checked');
+    }
+},false);
+
+// Creating a new list item on click of Add button
+function addNew(){
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === ''){
+        alert("Please enter some text for to-do!")
+    }
+    else{
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);  
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+// clearing all items from list
+function clearAll(){
+    var lst = document.getElementsByTagName("ul");
+    lst[0].innerHTML = "";
+}
